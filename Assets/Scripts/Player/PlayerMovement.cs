dc3013b01c1f05b4fcf13ff40c57234fb31fe3d1
DESTOUCHES Thibaud <tdestouc@emea.msad.sopra>
2017-04-22 00:25:13 +0200

    Add dynamic light + PlayerController

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+
+    private Vector2 _movement;
+    private Rigidbody2D _rigidbody;
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        Move(h);
+    }
+
+    private void Move(float h)
+    {
+        _movement.Set(h, 0);
+        _movement = _movement.normalized * Speed * Time.deltaTime;
+
+        Vector2 position = new Vector2(transform.position.x, transform.position.y);
+        _rigidbody.MovePosition(position + _movement);
+
+        _animator.SetFloat("XVelocity", h);
+    }
+}
