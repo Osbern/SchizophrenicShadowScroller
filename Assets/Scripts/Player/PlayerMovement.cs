3a73373ea94b96fd087aeded05ca9a86e58bcb0c
dudeare <boiteadude@orange.fr>
2017-04-22 13:22:28 +0200

    shadowball

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -12,16 +13,25 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D _rigidbody;
     private Animator _playerAnimator;
     private Animator _shadowAnimator;
-
+    private GameObject _shadowBall;
     //Timer
     private const float INPUT_DELAY = 0.5f;
+    private const float DISAPEARSPEED = 0.02f;
+    private const float DISAPEARRATE = 0.001f;
+    private const float MAXSHADOWSIZE = 0.5f;
     private float _inputTimer;
+
+    public bool IsActiveShadowBall, IsUnActiveShadowBall = false;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _playerAnimator = GetComponent<Animator>();
         _shadowAnimator = Shadow.GetComponent<Animator>();
+        _shadowBall = GetComponentsInChildren<Transform>().First(x => x.name == "shadowball").gameObject;
+        var particleColor = _shadowBall.GetComponent<ParticleSystem>().startColor;
+        _shadowBall.GetComponent<ParticleSystem>().startSize = 0;
+        _shadowBall.SetActive(false);
     }
 
     private void FixedUpdate()
@@ -45,12 +55,28 @@ public class PlayerMovement : MonoBehaviour
         }
 
         float h = Input.GetAxisRaw("Horizontal");
-        Vector2 movement = new Vector2(h, 0);
-        movement = movement.normalized * Speed * Time.deltaTime;
+        var v = Input.GetAxis("Vertical");
+
+        //ball or shadow
+        var activeShadowBall = v > 0;
+        if (_isShadowControlled && !_shadowBall.activeInHierarchy && activeShadowBall)
+        {
+            _shadowBall.transform.position = Shadow.transform.position;
+            if (!IsActiveShadowBall && !IsUnActiveShadowBall)
+            {
+                InvokeRepeating("ActiveShadowBall", 0, DISAPEARRATE);
+            }
+
+        }
+
+        Vector2 movement = new Vector2(h, _shadowBall.activeInHierarchy ? v : 0);
+        movement = movement.normalized * Speed * Time.deltaTime * (_shadowBall.activeInHierarchy ? 2 : 1);
 
         if (_isShadowControlled)
         {
-            if (h != 0)
+            _shadowBall.SetActive(!Shadow.activeInHierarchy);
+
+            if (h != 0 || v != 0)
                 MoveShadow(movement);
         }
         else
@@ -75,8 +101,74 @@ public class PlayerMovement : MonoBehaviour
 
     private void MoveShadow(Vector2 movement)
     {
-        Shadow.transform.localScale = (movement.x < 0) ? new Vector3(-1f, 1f) : new Vector3(1f, 1f);
+        //If up and light zone move shadow ball
+        if (_shadowBall.activeInHierarchy)
+        {
+            _shadowBall.transform.position += new Vector3(movement.x, movement.y);
+        }
+        else
+        {
+            //else move shadow
+            Shadow.transform.localScale = (movement.x < 0) ? new Vector3(-1f, 1f) : new Vector3(1f, 1f);
 
-        Shadow.transform.position += new Vector3(movement.x, movement.y);
+            Shadow.transform.position += new Vector3(movement.x, movement.y);
+        }
+
+    }
+
+    private void ActiveShadowBall()
+    {
+        _shadowBall.SetActive(true);
+        IsActiveShadowBall = true;
+
+        var startSize = _shadowBall.GetComponent<ParticleSystem>().startSize;
+        var newStartSize = startSize + DISAPEARSPEED;
+        _shadowBall.GetComponent<ParticleSystem>().startSize = Mathf.Clamp(newStartSize, 0, MAXSHADOWSIZE);
+
+        var newBodyScale = Mathf.Clamp(1 - (newStartSize / MAXSHADOWSIZE), 0, 1);
+        foreach (var item in Shadow.GetComponentsInChildren<Transform>())
+        {
+            item.localScale = new Vector3(newBodyScale, newBodyScale, newBodyScale);
+
+        }
+
+        if (newStartSize >= MAXSHADOWSIZE)
+        {
+            IsActiveShadowBall = false;
+            Shadow.SetActive(false);
+            CancelInvoke("ActiveShadowBall");
+        }
+
+    }
+
+    private void UnActiveShadowBall()
+    {
+        Shadow.SetActive(true);
+        IsUnActiveShadowBall = true;
+
+        var startSize = _shadowBall.GetComponent<ParticleSystem>().startSize;
+        var newStartSize = startSize - DISAPEARSPEED;
+        _shadowBall.GetComponent<ParticleSystem>().startSize = Mathf.Clamp(newStartSize, 0, MAXSHADOWSIZE);
+
+        var newBodyScale = Mathf.Clamp(1 - (newStartSize / MAXSHADOWSIZE), 0, 1);
+        foreach (var item in Shadow.GetComponentsInChildren<Transform>())
+        {
+            item.localScale = new Vector3(newBodyScale, newBodyScale, newBodyScale);
+
+        }
+
+        if (newStartSize <= 0)
+        {
+            IsUnActiveShadowBall = false;
+            _shadowBall.SetActive(false);
+            CancelInvoke("UnActiveShadowBall");
+        }
+
+    }
+
+    public void UnactiveBall()
+    {
+        if (!IsActiveShadowBall && !IsUnActiveShadowBall)
+            InvokeRepeating("UnActiveShadowBall", 0, DISAPEARRATE);
     }
 }
