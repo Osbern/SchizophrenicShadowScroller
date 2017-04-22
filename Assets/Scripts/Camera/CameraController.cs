8d2dd2304b952eebb315723b5edfd5377a91b737
DESTOUCHES Thibaud <tdestouc@emea.msad.sopra>
2017-04-22 19:54:20 +0200

    Level 01 lighting

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        Vector3 movement = new Vector3(h, 0);
+        movement = movement.normalized * 5 * Time.deltaTime;
+        transform.position += movement;
+    }
+}
