6e4dd71b10fc2e174bd11e74d2c8c0470543fd5a
dudeare <boiteadude@orange.fr>
2017-04-23 14:10:33 +0200

    Camera

--- Assets/Scripts/Camera/FollowCam.cs
+++ Assets/Scripts/Camera/FollowCam.cs
@@ -18,6 +18,7 @@ public class FollowCam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        transform.position = Vector3.Lerp(transform.position, Target.transform.position + _offset, Smooth);
 
     }
 }
